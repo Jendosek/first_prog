12d1b2470f9c558816cb2b04760c787661968266
Jendosek <bala01081977@gmail.com>
2023-04-25 22:04:44 +0300

    дз №1

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+#1
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+lst1 = [1, 3, 54, 6, 2 ,-12]
+def sum(lst):
+    sum = 0
+    for num in lst:
+        sum += num
+    return sum
+print(sum(lst1))
