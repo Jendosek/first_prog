2b3ea5d2e30a17e0a3efd20cd6a425bd147131e9
student <nataliyalednevashukina@gmail.com>
2023-04-25 20:30:41 +0300

    дргий приклад

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
- #теорія
- #Задано дві змінні a і b зі значеннями 5 та 10
- #відповідно. Напишіть функцію swap(a, b), яка обмінює
- #значеннями змінних a і b. Напишіть код, що демонструє
- #роботу функції та показує значення змінних a і b до та
- #після виклику функції.
-a = 5
-b = 10
-def swap(a,b):
-    a,b = b,a
-    return a,b
-print(f"До обміну: a = {a}, b = {b}")
-a, b = swap(a, b)
-print(f"Після обміну: a = {a}, b = {b}")
+strings = ['Hello', 'lol', 'Ololo lolo']
+#s = input("Що перевірить")
+def  count_vowels(s):
+    vowels = 'eyuioaQEYUIOA'
+    count = 0
+    for letters in s:
+        if letters in vowels:
+            count += 1
+    return count
+for s in strings:
+    print(f"У рядку {s} голосних літер {count_vowels(s)}")
