26247e3647a54a4db1e534be0a6249e980c451f0
student <nataliyalednevashukina@gmail.com>
2023-04-25 20:48:23 +0300

    третій приклад приклад

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-strings = ['Hello', 'lol', 'Ololo lolo']
-#s = input("Що перевірить")
-def  count_vowels(s):
-    vowels = 'eyuioaQEYUIOA'
-    count = 0
-    for letters in s:
-        if letters in vowels:
-            count += 1
-    return count
-for s in strings:
-    print(f"У рядку {s} голосних літер {count_vowels(s)}")
+print("hello wrld")
