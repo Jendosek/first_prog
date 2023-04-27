8a9e87fd18e8b29d25f8023800b8c3fa8d44d3d7
student <nataliyalednevashukina@gmail.com>
2023-04-27 18:24:26 +0300

    завадння №2

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-#1
-a = 5
-b = 10
-def swap(a, b):
-    a, b = b, a
-    return a, b
-print(f"До обміну: a = {a}, b = {b}")
-a, b = swap(a, b)
-print(f"Після обміну: a = {a}, b = {b}")
-
+#2
+def  count_vowels(s):
+    vowels = 'aeiouyAEIOUY'
+    count = 0
+    for letters in s:
+        if letters in vowels:
+            count += 1
+    return count
+s1 = input("Введіть рядок: ")
+print(count_vowels(s1))
