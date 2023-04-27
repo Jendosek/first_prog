b14d8835c6a6a64b9304c2ad4a0bd4dc26809c0a
Jendosek <bala01081977@gmail.com>
2023-04-27 22:24:56 +0300

    завдання №4

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-#4
-def func_4(*args):
-    dict1 = {}
-    for name, age in args:
-        dict1[name] = age
-    return dict1
-print(func_4(*[('Женя', 16), ('Захар', 20), ('Влад', 15), ('Андре', 30)]))
+#1
+def string(*args):
+    if not args:
+        return None
+    for arg in args:
+        print(arg, end=", ")
+    return args
+string("Hello", "my", "new","programme")
