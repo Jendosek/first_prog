3dfb25c259b7da460aaa3abce56c64a1cb5a09f0
student <nataliyalednevashukina@gmail.com>
2023-04-27 20:20:01 +0300

    завдання №2

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-#1
-def func_1(*args):
-    count = 0
-    for argument in args:
-        count += argument
-        a = count / 5
-    print(a)
-func_1(2, 5, 6, 7, 13)
+#2
+def func_2(*args):
+    
