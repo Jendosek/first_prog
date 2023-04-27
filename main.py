0ed8158c421de32bd139d3cf10f2cf78d921a856
student <nataliyalednevashukina@gmail.com>
2023-04-27 19:13:56 +0300

    аудиторна №1

--- main.py
+++ main.py
@@ -48,3 +48,16 @@ def black_hole_full_2(var1, *args, **kwargs):
     for key, value in kwargs.items():
         print(key, value)
 black_hole_full_2(234, "Earth", "russia", "time", 346, name="Gleb", planet="Earth", food="Hleb", number = 5)
+
+print()
+
+#6
+lst = [2,3,4,5,6,7]
+dicd = {"n": 1, "b": 2, "s": 3}
+def fun(var_1, var_2, var_3, var_4, var_5, var_6):
+    return var_1, var_2, var_3, var_4, var_5, var_6
+print(fun(*lst))
+
+def fun_dict(n, b, s):
+    return n, b, s
+print(fun_dict(**dicd))
