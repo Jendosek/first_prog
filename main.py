06ea9597f5199113e2c70d4545350905e721cba1
student <nataliyalednevashukina@gmail.com>
2023-04-27 18:29:18 +0300

    дз №2

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-#1
-lst1 = [1, 3, 54, 6, 2 ,-12]
-def sum(lst):
-    sum = 0
-    for num in lst:
-        sum += num
-    return sum
-print(sum(lst1))
+#2
+def string(s):
+    s = s.lower()
+    s = s.replace(" ", "")  # Видаляємо всі пробіли з рядка
+    if s == s[::-1]:
+        print("True")
+    else:
+        print("False")
+s1 = input("Введіть рядок: ")
+string(s1)
+
