18de1b10cebc6257598b3c9a686d178670b4b7fe
student <nataliyalednevashukina@gmail.com>
2023-04-27 18:48:31 +0300

    аудиторна №1

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+#1
+def black_hole(*args):
+    print(type(args))
+    print(args)
+    for argument in args:
+        print(argument)
+black_hole(234, "Earth", "russia", "time")
+
+print()
+
+#2
+def black_hole_named(**kwargs):
+    print(type(kwargs))
+    for key, value in kwargs.items():
+        print(key, value)
+black_hole_named(name="Gleb", planet="Earth", food="Hleb", number = 5)
+
+print()
+
 #3
-def sort(strings):
-    return sorted(strings, key=len)
-strings1 = input("Введіть рядок: ").split(' ')
-strings1 = list(strings1)
-x = sort(strings1)
-print(x)
-
-
+def black_hole_named(**kwargs):
+    print(type(kwargs))
+    print(kwargs)
+    for argument in kwargs:
+        print(argument)
+black_hole_named(name="Gleb", planet="Earth", food="Hleb", number = 5)
