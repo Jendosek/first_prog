3d224ce35bdf78f4111dbb0f49a0f356f924c99f
student <nataliyalednevashukina@gmail.com>
2023-05-02 20:42:27 +0300

    завдання №3

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-#2
-import time
-def time_function(func):
-    def wrapper (*args, **kwargs):
-        stat_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        x = end_time - stat_time
-        print("Час виконання функції:", round(x, 2), "секунд")
-        return result
+#3
+def cache(func):
+    dict_1 = {}
+    def wrapper(*args, **kwargs):
+        if args in dict_1:
+            return dict_1[args]
+        else:
+            res = func(*args)
+            dict_1[args] = res
+            return res, dict_1
     return wrapper
-@time_function
-def example_function(x):
-    time.sleep(2)
-    return x
-print(example_function("Hello World!"))
+@cache
+def example_function(x,y):
+    return x * y
+print(example_function(3,10))
