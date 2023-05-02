6a5515051365334bdd109f0ba9f55457f31ac6fe
student <nataliyalednevashukina@gmail.com>
2023-05-02 19:17:15 +0300

    аудиторна №2

--- main.py
+++ main.py
@@ -11,5 +11,33 @@ def say_hello(): #1
     print("Hello!")
 say_hello()
 
-#2
+print()
 
+#2
+"""import time
+def delay_decorator(func):
+    def wrapper(*args, **kwargs):
+        time.sleep(3)
+        return func(*args, **kwargs)
+    return wrapper
+@delay_decorator
+def sleepy():
+    print("я сплю")
+sleepy()
+
+print()"""
+
+#3
+def cache_decorator(func):
+    cache = {}
+    def wrapper(n):
+        if n not in cache:
+            cache[n] = func(n)
+        return cache[n]
+    return wrapper
+@cache_decorator
+def fibonacci(n):
+    if n in (0, 1):
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+print(fibonacci(10))
