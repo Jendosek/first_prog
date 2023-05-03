449c4c433e9cf655d82587255feb91d6403170f1
Jendosek <bala01081977@gmail.com>
2023-05-03 22:26:53 +0300

    завдання №3

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-#2
-def retur(func):
-    def wrapper(*args, **kwargs):
-        x = func(*args, **kwargs) + 100
-        print(x)
+#3
+def loggging(func):
+    def wrapper(t):
+        result = func(t)
+        print(f"Температура: {t} градусів Цельсія, становить {round(result)} градусів по Фаренгейту")
+        return result
     return wrapper
-@retur
-def dobutok(x):
-    res = 1
-    for args in x:
-        res *= args
-    return res
-lst = [1, 2, 3, 4, 5]
-dobutok(lst)
+@loggging
+def example_func(t):
+    return (t * 1.8) + 32
+tempereture = 10
+example_func(tempereture)
