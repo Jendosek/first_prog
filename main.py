0b9cf36bddbff5e23c1f38a81a32c51f8da3eaa3
Jendosek <bala01081977@gmail.com>
2023-05-03 22:50:27 +0300

    завдання №4

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-#1
+#4
 import time
+
 def timer(func):
-    def wrapper(*args):
-        res = func(args)
-        for i in range(1,6):
-            if 0 < i < 6:
-                time.sleep(3)
-                print(res)
-        return res
+    def wrapper(num):
+        start_time = time.time() #Початок
+        result = func(num) #Виконання
+        end_time = time.time() #Завершення
+        t = end_time - start_time
+        print("Пройшло:", round(t, 2))
+        return result
     return wrapper
+
 @timer
-def example_fuck(x):
-    return x
-example_fuck("Hello World!")
+def numbersss(num):
+    time.sleep(2)
+    x = 'Hе натуральне'
+    y = 'Hатуральне'
+    if num < 2:
+        return x
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return x
+    return y
+number_1 = 3
+number_2 = 10
+print(f"Число {number_1}: {numbersss(number_1)}")
+print(f"Число {number_2}: {numbersss(number_2)}")
