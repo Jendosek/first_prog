d1126184eacc8c200cf1c5e35168153cfc903786
Jendosek <bala01081977@gmail.com>
2023-05-03 22:18:01 +0300

    завдання №2

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-#1
-#xz
+#2
+def retur(func):
+    def wrapper(*args, **kwargs):
+        x = func(*args, **kwargs) + 100
+        print(x)
+    return wrapper
+@retur
+def dobutok(x):
+    res = 1
+    for args in x:
+        res *= args
+    return res
+lst = [1, 2, 3, 4, 5]
+dobutok(lst)
