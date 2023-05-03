6cfa19d97eda2c1701df1eac761781d4030a69d9
Jendosek <bala01081977@gmail.com>
2023-05-03 22:05:10 +0300

    завдання №1

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-#4
-def log(func):
-    def wrapper(*args, **kwargs):
-        res = func(*args, **kwargs)
-        print(f"Функція: {func.__name__}, Аргументи: {args}, Результат: {res}")
-        return res
-    return wrapper
-@log
-def example_func(x, y):
-    return x * y
-print(example_func(5, 7))
-
+#1
+#xz
