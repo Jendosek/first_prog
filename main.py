cd1e4f97e7ff92b3908ef05bb9fcc7fb83f88455
student <nataliyalednevashukina@gmail.com>
2023-05-04 20:16:10 +0300

    завдання №1

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-#   classes
+#1
 class Student:
-    print("Hi!")
-    count = 0
-    def __init__(self, height = 150):
-        self.height = height
-        self.text = "I`m shy"
-        Student.count += 1
-    def brething(self, height):
-        return self.height - 10
-oleg = Student() #Обєкт екземпляру класу
-print(oleg.height)
-masha = Student(height=200)
-print(masha.height)
-print(Student.count)
-print(masha.brething(None))
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+print_info = Student(name="Zhenia", age = 16)
+print(f"Ім`я студента: {print_info.name}")
+print(f"Вік студента: {print_info.age} років")
