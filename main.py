ec5ac31dfca018f7c2e12572e4a1b6551964f3ed
student <nataliyalednevashukina@gmail.com>
2023-05-04 19:20:35 +0300

    аудиторна №3

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #   classes
 class Student:
     print("Hi!")
+    count = 0
     def __init__(self, height = 150):
         self.height = height
         self.text = "I`m shy"
+        Student.count += 1
 oleg = Student() #Обєкт екземпляру класу
 print(oleg.height)
 masha = Student(height=200)
 print(masha.height)
+print(Student.count)
