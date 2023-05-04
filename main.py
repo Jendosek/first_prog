e7b67ead0453d48e84e162f98852120b771f73c8
student <nataliyalednevashukina@gmail.com>
2023-05-04 18:46:31 +0300

    аудиторна №3

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
-#3
-def cache(func):
-    dict_1 = {}
-    def wrapper(*args, **kwargs):
-        if args in dict_1:
-            return dict_1[args]
-        else:
-            res = func(*args)
-            dict_1[args] = res
-            return res, dict_1
-    return wrapper
-@cache
-def example_function(x,y):
-    return x * y
-print(example_function(3,10))
+#   classes
+
