77678ae84a6c7540c1336388df0cd4666d19c08c
student <nataliyalednevashukina@gmail.com>
2023-05-04 20:28:23 +0300

    завдання №2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-#1
-class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-print_info = Student(name="Zhenia", age = 16)
-print(f"Ім`я студента: {print_info.name}")
-print(f"Вік студента: {print_info.age} років")
+#2
+import math
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * (self.radius ** 2)
+name = Circle(radius = 10)
+print(f"Радіус кола: {name.radius}")
+print(f"Площа кола: {round(name.area(), 2)} см^2")
