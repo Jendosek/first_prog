9c1067b48f954d11fcc1aaa40fc886e3d9a9025c
student <nataliyalednevashukina@gmail.com>
2023-05-04 20:51:57 +0300

    завдання №4

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-#3
-class Animals:
-    pass
-class Dog(Animals):
-    def sound(self):
-        print("Собака каже: Гав")
-dogi = Dog()
-dogi.sound()
+#4
+class BankAccount:
+    def __init__(self, balance, owner):
+        self.balance = balance
+        self.owner = owner
+    def deposit(self, all):
+        self.all = all
+    def withdraw(self, dep):
+        self.dep = dep
+name = BankAccount(balance=1000, owner=100)
+print(name.all)
+
