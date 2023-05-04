36d1f20e1563e392a70d49e20a1036541e2fba7e
student <nataliyalednevashukina@gmail.com>
2023-05-04 19:49:39 +0300

    аудиторна №3

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ class Student:
         self.height = height
         self.text = "I`m shy"
         Student.count += 1
+    def brething(self, height):
+        return self.height - 10
 oleg = Student() #Обєкт екземпляру класу
 print(oleg.height)
 masha = Student(height=200)
 print(masha.height)
 print(Student.count)
+print(masha.brething(None))
