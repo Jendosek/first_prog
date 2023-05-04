06d04abb94fd216b8ca14870d983cde4b404ba50
Jendosek <bala01081977@gmail.com>
2023-05-04 23:46:17 +0300

    завдання №4

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 #4
-class BankAccount:
-    def __init__(self, owner, balance = 0):
-        self.owner = owner
+class Bank:
+    def __init__(self, name, balance):
+        self.name = name
         self.balance = balance
-    def deposit(self, amount):
-        self.balance += amount
-    def withdraw(self, amount):
-        if self.balance - amount < 0:
-            print("Недостатньо коштів")
-        else:
-            self.balance -= amount
-    def check_balance(self):
-        return self.balance
-name = BankAccount(owner = "Zhenia", balance = 1000)
-name.deposit(300)
-name.withdraw(100)
-print(name.check_balance())
+    def add_customer(self, customer):
+        self.customer = customer
+
+result = Bank("Zhenia", 1000)
+print(result.name)
+print(result.balance)
+result.add_customer("dod")
+
 
