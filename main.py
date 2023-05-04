76ba5e7c8ac3c447e0be4067afd79ec0f49019b9
Jendosek <bala01081977@gmail.com>
2023-05-04 22:07:36 +0300

    завдання №4

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
 #4
-import time
+class BankAccount:
+    def __init__(self, owner, balance = 0):
+        self.owner = owner
+        self.balance = balance
+    def deposit(self, amount):
+        self.balance += amount
+    def withdraw(self, amount):
+        if self.balance - amount < 0:
+            print("Недостатньо коштів")
+        else:
+            self.balance -= amount
+    def check_balance(self):
+        return self.balance
+name = BankAccount(owner = "Zhenia", balance = 1000)
+name.deposit(300)
+name.withdraw(100)
+print(name.check_balance())
 
-def timer(func):
-    def wrapper(num):
-        start_time = time.time() #Початок
-        result = func(num) #Виконання
-        end_time = time.time() #Завершення
-        t = end_time - start_time
-        print("Пройшло:", round(t, 2))
-        return result
-    return wrapper
-
-@timer
-def numbersss(num):
-    time.sleep(2)
-    x = 'Hе натуральне'
-    y = 'Hатуральне'
-    if num < 2:
-        return x
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return x
-    return y
-number_1 = 3
-number_2 = 10
-print(f"Число {number_1}: {numbersss(number_1)}")
-print(f"Число {number_2}: {numbersss(number_2)}")
