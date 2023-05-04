36ab1de5b139bda1881fd86ad00767f68d95b30e
Jendosek <bala01081977@gmail.com>
2023-05-04 22:08:38 +0300

    завдання №4

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class BankAccount:
             self.balance -= amount
     def check_balance(self):
         return self.balance
-name = BankAccount(owner = "Zhenia", balance = 1000)
+name = BankAccount(owner = "Zhena", balance = 1000)
 name.deposit(300)
 name.withdraw(100)
 print(name.check_balance())
