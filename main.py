db65e33616146b79b36ae85198cba638120474e9
Jendosek <bala01081977@gmail.com>
2023-05-05 22:03:33 +0300

    завдання №3

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-#2
-class Rectangle:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.result_1 = width * height
-        self.result_2 = 2 * (width + height)
-name = Rectangle(width = 10, height = 6)
-print(f"Довжина = {name.width}")
-print(f"Висота  = {name.height}")
-print("--------------------------")
-print(f"Периметр прямокутника: {name.result_2}")
-print(f"Площа прямокутника: {name.result_1}")
+#3
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+name = Car(brand = "Lamborghini",
+           model = "Huracan",
+           year = 2014)
+print(f"Марка: {name.brand}")
+print(f"Модель: {name.model}")
+print(f"Рік: {name.year}")
