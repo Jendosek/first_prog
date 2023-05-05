acfb33dde7dac0e835fe55e304c3ef9a7621d35f
Jendosek <bala01081977@gmail.com>
2023-05-05 22:49:50 +0300

    завдання №4

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-#3
-class Car:
-    def __init__(self, brand, model, year):
-        self.brand = brand
-        self.model = model
-        self.year = year
-name = Car(brand = "Lamborghini",
-           model = "Huracan",
-           year = 2014)
-print(f"Марка: {name.brand}")
-print(f"Модель: {name.model}")
-print(f"Рік: {name.year}")
+#4
+class Bank:
+    def __init__(self, customer, blnce):
+        self.customer = customer
+        self.blnce = blnce
+class BankAccount:
+    def __init__(self, customers, balance):
+        self.customers = customers
+        self.balance = balance
+    def get_total_balance(self, blnce, balance):
+        x =  blnce + balance
+        return x
+name = Bank("Zhenia", 1000)
+name1 = Bank("Zahar", 600)
+name2 = Bank("Sasha", 180)
+
+print(f"Клієнт: {name.customer}, Баланс {name.blnce} грн")
+print(f"Клієнт: {name1.customer}, Баланс {name1.blnce} грн")
+print(f"Клієнт: {name2.customer}, Баланс {name2.blnce} грн")
+print("-------------------------------------")
+name_res = BankAccount("Gleb", 120)
+print(f"Новий клієнт: {name_res.customers}, Баланс {name_res.balance} грн")
+print("-------------------------------------")
+print(f"Загальний баланс усіх клієнтів становить: {name_res.get_total_balance(blnce = 1780, balance = 120)} грн")
+
