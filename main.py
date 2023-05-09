768eb348f799ca0b152311c207edff113fa92648
student <nataliyalednevashukina@gmail.com>
2023-05-09 19:20:28 +0300

    аудиторна №4

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ class School:
     def admit_student(self, student):
         self.students.append(student)
         print(f'{student.name} був допущений до школи {self.name}')
+    def expel_student(self, student):
+        """expelled_student ="""
+        pass
 
 class Student:
     def __init__(self, name, grade):
@@ -18,3 +21,13 @@ class Student:
     def demote(self):
         self.grade -= 1
         print(f'{self.name} був понижений за гру в шахи {self.grade}')
+    def __str__(self):
+        return f'{self.name} - Ранг {self.grade}'
+
+
+
+"""multiply = lambda x, y: x*y
+print(multiply(2,5))
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+filtered_numbers = list(filter(lambda a: a % 2 == 0, numbers))
+print(filtered_numbers)"""
