91fb4c5783d51e081c0c69b88d4e1172d981391f
student <nataliyalednevashukina@gmail.com>
2023-05-09 18:56:55 +0300

    аудиторна №4

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ class School:
         self.students = students  #список
     def admit_student(self, student):
         self.students.append(student)
-        print(f'{}')  #дописати коли створимо клас студентів
+        print(f'{student.name} був допущений до школи {self.name}')
 
-class Students
+class Student:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+    def promote(self):
+        self.grade += 1
+        print(f'{self.name} був підвищений {self.grade}')
+    def demote(self):
+        self.grade -= 1
+        print(f'{self.name} був понижений за гру в шахи {self.grade}')
