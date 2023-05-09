7993e0367b142ec9bb9d65c71dd87dd1b5889796
student <nataliyalednevashukina@gmail.com>
2023-05-09 18:45:24 +0300

    аудиторна №4

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-#4
-class BankAccount:
-    def __init__(self, balance, owner):
-        self.balance = balance
-        self.owner = owner
-    def deposit(self, all):
-        self.all = all
-    def withdraw(self, dep):
-        self.dep = dep
-name = BankAccount(balance=1000, owner=100)
-print(name.all)
+#///classes///dubl_2
+#1  симулятор навчального закладу
+class School:
+    def __init__(self, name, students):
+        self.name = name
+        self.students = students  #список
+    def admit_student(self, student):
+        self.students.append(student)
+        print(f'{}')  #дописати коли створимо клас студентів
 
+class Students
