f8d630635f70e62a3c8639f8a08e81e79d86cb3b
student <bala01081977@gmail.com>
2023-05-16 19:45:59 +0300

    аудиторна №6

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 #1
-try:
+"""try:
     print("Start")
-    print(error)
+    print("error")
     print("Finish")
 except:
     print("bro, it`s error💀💀💀")
-print("let`s get it")
+print("let`s get it")"""
 
 #2
+"""def checker(var_1):
+    if type(var_1) != str:
+        raise TypeError(f"Sorry!, ми не можемо працювати з класом {type(var_1)} будь ласка введіть в str")
+    else:
+        return var_1
+a = 1234
+checker(a)
+print()
+print()"""
+
+#3
+class BuildingError(Exception):
+    def __str__(self):
+        return "шото багато"
+def chech_material(amount, limit):
+    if amount > limit:
+        return "Достатньо матеріалів"
+    else:
+        raise BuildingError()
+print(chech_material(123,300))
