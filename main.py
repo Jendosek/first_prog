8cde493cb1180b8161f8abfd8411763fd70f1e2f
student <bala01081977@gmail.com>
2023-05-18 20:23:00 +0300

    завдання №2

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-#1
-class OddIterator:
-    def __init__(self, name):
-        self.name = name
-        self.index = 0
-    def __iter__(self):
-        return self
-    def __next__(self):
-        if self.index >= len(self.name):
-            raise StopIteration
-        if self.index < 0:
-            raise ValueError("Bro 💀")
-        value = self.name[self.index]
-        self.index += 2
-        return value
-my_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-my_iter = OddIterator(my_list)
-for num in my_iter:
-    print(num)
+#2
+my_list = [1,2,3,4,5,6,7,8,9]
+class SquareGenerator:
+    def __init__(self):
+        if isinstance(my_list, list) is True:
+            print(my_list)
+        elif isinstance(my_list, list) is not True:
+            TypeError("Brroo 💀")
+    def my_generator(self, name):
+        for num in my_list:
+            num = num**2
+            yield num
+    for num in my_generator("", my_list):
+        print(num)
