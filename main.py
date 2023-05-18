2d6d96ac54765a580fc97954f8f6623f9ead5857
student <bala01081977@gmail.com>
2023-05-18 20:50:15 +0300

    завдання №4

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-#1
-class OddIterator:
-   def __init__(self, n):
-       if not isinstance(n, int) or n <= 0:
-           raise ValueError("треба n ціле натуральне число")
-       self.n = n
-       self.current = 1
-   def __iter__(self):
-        return self
-   def __next__(self):
-        if self.current > self.n:
-            raise StopIteration
-        else:
-            result = self.current
-            self.current +=2
-            return result
-#приклад використання
-try:
-    for num in OddIterator([1,2,3]):
-        print(num)
-except ValueError as e:
-    print (e)
+#4
+def multiply_by():
+    def mnog(a, b):
+        return a * b
+    return mnog
+multile = multiply_by()
+print(multile(3,6))
