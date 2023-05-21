4a23667db33fa3ebc88807933c51cbba9758f73a
Jendosek <bala01081977@gmail.com>
2023-05-21 21:44:36 +0300

    Завдання №2

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-#3
-import random
-class PasswordGenerator:
-    def __init__(self, lenth, symbols):
-        self.lenth = lenth
-        self.symbols = symbols
-    def genarate_password(self):
-        password = ''
-        for i in range(self.lenth):
-            password += random.choice(self.symbols)
-        return password
-generator = PasswordGenerator(10, 'qwertyQWERTY1234!@#$%^_-.')
-for i in range(3):
-    print(f"Ваш пароль: {generator.genarate_password()}")
+#2
+def average_closure():
+    numbers = []
+    def add_number(number):
+        numbers.append(number)
+    def get_average():
+        print(numbers)
+        return sum(numbers) / len(numbers)
+    return add_number, get_average
+add_number, get_average = average_closure()
+add_number(1)
+add_number(2)
+add_number(3)
+add_number(4)
+add_number(5)
+add_number(6)
+add_number(7)
+add_number(8)
+add_number(9)
+print(f"Середнє число зі списку: {get_average()}")
+
