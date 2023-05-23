48dc3364aaa46a62aa5bc05756850fc189e6bb0e
student <bala01081977@gmail.com>
2023-05-23 20:25:10 +0300

    завдання №2

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ while logger != "q":
                     filename= "logss.log",
                     filemode= 'w',
                     format='%(asctime)s - %(levelname)s - %(message)s')
-    logging.info(f"інфформація {logger} була додана до файлу")
+    logging.info(f"Інфформація {logger} була додана до файлу")
     for i in logger:
         if i == "1" or i == "2" or i == "3" or i == "4" or i == "5" or i == "6" or i == "7" or i == "8" or i == "9" or i == "0":
             logging.error(f"число ({i}) вводити неможна")
+    with open("only.txt", 'w') as file:
+        file.write(logger)
