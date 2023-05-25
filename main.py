8f4171f4976b3237cea770ce0c26cc24e99654b0
student <bala01081977@gmail.com>
2023-05-25 19:50:41 +0300

    завдання №1

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get("https://www.example.com/")
-if response.status_code == 200:
+try:
     soup = BeautifulSoup(response.content, "html.parser")
     title = soup.find("title").text
     print(title)
-else:
+except:
     print(f"Немає підключення {response.status_code}!")
