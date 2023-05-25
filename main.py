25bcbdbc6429996ff6b0d167f62ba74db1ad9fb2
Jendosek <bala01081977@gmail.com>
2023-05-25 23:06:23 +0300

    Завдання №4

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-#3
+#4
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://uk.wikipedia.org/wiki/")
+response = requests.get("https://www.example.com")
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, "html.parser")
-    classs = soup.find_all("exampel_class")
-    for i in classs:
-        print(i)
+    res = soup.find_all('h1')
+    for i in res:
+        print(res)
 else:
     print(f"немає підклчення {response.status_code}")
