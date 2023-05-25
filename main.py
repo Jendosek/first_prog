fc8e2e0afc4fe574b7586bc2abed158b7f85d983
student <bala01081977@gmail.com>
2023-05-25 18:46:21 +0300

    аудиторна №9

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-#3
-import random
-import logging
-
-def last_task(file_path, num):
-    try:
-        with open(file_path, 'w') as file:
-            for i in range(num):
-                rad = random.randint(1,100)
-                file.write(str(rad) + '\n')
-                logging.info(f"Рандомне число: {rad}")
-    except Exception as e:
-        logging.error(f"error: {e}")
-file_path = "input_random.txt"
-last_task("input_random.txt",  input("Кількість генерованих чисел: "))
+# audit_work #
+import urllib.request
+opener = urllib.request.build_opener()
+response = opener.open("https://httpbin.org/get")
+print(response.read())
